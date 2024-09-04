ffca020e7b00e224eb3b59cc4d5773d05c7e2423
MariuszMalankiewicz <m2ario123@wp.pl>
2024-09-04 15:22:45 +0200

    Fix: NavigationComponent
    minor responsiveness fixes

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,16 +1,16 @@
 <template>
-    <nav class=" w-fit max-w-[1440px] mx-auto h-79 py-[16px]">
-        <div class="h-47 grid grid-cols-2 md:grid-cols-3 gap-0 lg:gap-[10px]">
-            <div class="order-1 w-[300px] h-[29.58px]">
-                <img class="w-[152.79px] h-[32.61px] mt-[0.79px] ml-[16px] lg:ml-0" src="../assets/Group 255.png">
+    <nav class="Navigations h-79 py-[16px] relative z-10">
+        <div class="Frame 4090 w-full px-4 md:w-3/4 h-47 mx-auto flex flex-col sm:flex-row items-center sm:justify-between gap-[10px]">
+            <div class="Frame 40567 w-[300px] h-[29.58px]">
+                <img class="Group 255 w-[152.79px] h-[32.61px] mt-[0.79px] mx-auto sm:mx-0" src="../assets/Group 255.png" alt="logo called cars spot">
             </div>
-            <div class="order-3 md:order-2 mt-2 w-[144px] h-[24px] grid grid-cols-2 gap-24 ml-[16px] lg:ml-0">
-                <p class="w-[91px] h-[24px] font-roboto font-normal text-[14px] leading-[24px] text-center text-black">Galeria zdjęć</p>
-                <p class="w-[29px] h-[24px] font-roboto font-normal text-[16px] leading-[24px] text-center text-black">FaQ</p>
+            <div class="Frame 40566 mt-2 w-[144px] h-[24px] flex gap-[24px] mt-2 mb-2 sm:mt-0 sm:mb-0">
+                <p class="Galeria zdjęć w-[91px] h-[24px] font-roboto font-normal text-[14px] leading-[24px] text-center text-black">Galeria zdjęć</p>
+                <p class="FaQ w-[29px] h-[24px] font-roboto font-normal text-[16px] leading-[24px] text-center text-black">FaQ</p>
             </div>
-            <div class="order-2 md:order-3 w-[300px] h-[47] grid gap-10 sm:justify-start md:justify-center xl:justify-end">
-                <button class="w-[158px] h-[47] rounded-[8px] p-[8px] bg-btn-blue">
-                    <p class="font-roboto font-semibold text-[15px] leading-[22.5px] tracking--2 text-center text-white">Zadzwoń do nas</p>
+            <div class="Frame 40568 w-[300px] h-[47px] flex gap-[10px] justify-center sm:justify-end">
+                <button class="Buttons w-[158px] h-[47px] rounded-[8px] p-[8px] bg-blue">
+                    <p class="CTA font-roboto font-semibold text-[15px] leading-[22.5px] tracking--2 text-center text-white">Zadzwoń do nas</p>
                 </button>
             </div>
         </div>
