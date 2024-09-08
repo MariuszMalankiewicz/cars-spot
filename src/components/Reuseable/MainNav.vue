063a28057d7e4417ec3ca591f37d226feb8bcbfe
MariuszMalankiewicz <m2ario123@wp.pl>
2024-09-08 18:36:00 +0200

    feat: add buttons components
    add primary and secondary buttons with slot for dynamic content.

--- src/components/Reuseable/MainNav.vue
+++ src/components/Reuseable/MainNav.vue
@@ -1,3 +1,7 @@
+<script setup>
+import Button from './Buttons/PrimaryButton.vue'
+</script>
+
 <template>
     <nav class="py-[16px] relative z-10">
         <div class="mx-auto w-[95%] lg:w-[90%] xl:w-[80%] px-4 flex flex-wrap items-center justify-center sm:justify-between gap-x-[24px] gap-y-[12px] sm:gap-[10px]">
@@ -9,9 +13,7 @@
                 <p class="font-roboto font-normal text-[16px] leading-[24px] text-center cursor-pointer text-dark">FaQ</p>
             </div>
             <div class="flex gap-[10px] justify-center sm:justify-end">
-                <button class="rounded-[8px] py-[10px] px-[18px] bg-btn-primary">
-                    <p class="font-roboto font-semibold text-[15px] leading-[22.5px] tracking--2 text-center text-btn-secondary">Zadzwoń do nas</p>
-                </button>
+                <Button>Zadzwoń do nas</Button>
             </div>
         </div>
     </nav>
--- src/components/layouts/Hero.vue
+++ src/components/layouts/Hero.vue
@@ -1,3 +1,8 @@
+<script setup>
+import PrimaryButton from '../Reuseable/Buttons/PrimaryButton.vue'
+import SecondaryButton from '../Reuseable/Buttons/SecondaryButton.vue'
+</script>
+
 <template>
     <section class="h-[825px] py-[30px] sm:py-[120px] gap-[48px] relative">
         <div class="w-[668.05px] h-[659px] absolute top-[-170px] sm:top-[-80px] right-[3%] sm:right-[-40%] md:right-[-15%] lg:right-[0%] xl:right-[10%] 2xl:right-[5%]">
@@ -9,9 +14,9 @@
             <p class="h-[48px] font-robotoCondensed font-normal text-[16px] leading-[24px] text-dark">Kup komfortowy pojazd, aby każda podróż 
               <span class="block">była wyjątkowym przeżyciem.</span></p>
           </div>
-          <div class="h-[47px] flex gap-[12px] sm:gap-[24px] ">
-            <button class="w-[148px] h-[47px] rounded-[8px] bg-btn-primary"><p class="h-[23px] font-robotoFlex font-semibold text-[15px] leading-[22.5px] tracking-[-2%] text-center text-btn-secondary">Zobacz zdjęcia</p></button>
-            <button class="w-[158px] h-[47px] border-[1px]  border-btn-primary bg-light rounded-[8px]"><p class="h-[23px] font-robotoFlex font-semibold text-[15px] leading-[22.5px] tracking-[-2%] text-center text-btn-primary">Zadzwoń do nas</p></button>
+          <div class="h-[47px] flex gap-[12px] sm:gap-[24px]">
+            <PrimaryButton>Zobacz zdjęcia</PrimaryButton>
+            <SecondaryButton>Zadzwoń do nas</SecondaryButton>
           </div>
         </div>
         <div class="w-[700px] lg:w-[900px] xl:w-[1200px] 2xl:w-[1450px] h-[404.37px] absolute top-[45%] right-[0%] overflow-hidden">
